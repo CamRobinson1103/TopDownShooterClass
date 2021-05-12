c8e27350a6caa1d3878a41ce4608fbee7b9cb2d4
CamRobinson <robinsoncameron1103@gmail.com>
2021-05-12 11:55:18 -0500

    Followed Instructiond

---
+++ Assets/Scripts/Scriptable Objects/SpawnManagerScriptableObject.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New SpawnManagr", menuName = "AI/SpawnMenu")]
+public class SpawnManagerScriptableObject : ScriptableObject
+{
+
+    public string prefabName;
+    public int spawnCount;
+    public float timeBetweenSpawns;
+
+    
+}
